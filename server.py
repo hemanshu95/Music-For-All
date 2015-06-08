ce812a027d4ae8fc093f18e1889bbdf1a6c264dc
hemanshu95 <hemanshu95@gmail.com>
2015-06-08 23:55:32 +0530

    Done almost backend

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import os
 # will be used to redirect the user once the upload is done
 # and send_from_directory will help us to send/show on the
 # browser the file that the user just uploaded
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory,jsonify
 from werkzeug import secure_filename
 
 # Initialize the Flask application
@@ -14,6 +14,8 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 # These are the extension that we are accepting to be uploaded
 app.config['ALLOWED_EXTENSIONS'] = set(['mp3'])
+music_files = []
+music_files_number = 0
 
 # For a given file, return whether it's an allowed type or not
 def allowed_file(filename):
@@ -23,23 +25,34 @@ def allowed_file(filename):
 # This route will show a form to perform an AJAX request
 # jQuery is loaded to execute the request and update the
 # value of the operation
+
+
 @app.route('/')
 def index():
-    music_files = [f for f in os.listdir('uploads/') if f.endswith('mp3')]
-    music_files_number = len(music_files)
+    global music_files, music_files_number
+  #  music_files= [f for f in os.listdir('uploads/') if f.endswith('mp3')]
+   # music_files_number = len(music_files)
 
     return render_template('index.html',
-    title = 'Home',
-    music_files_number = music_files_number,
-    music_files = music_files,
-    ip_addr =  request.remote_addr)
+                           title='Home',
+                           music_files_number=music_files_number,
+                           music_files=music_files,
+                           ip_addr=request.remote_addr)
 
 
-# Route that will process the file upload
+
+
+
+
 @app.route('/upload', methods=['POST'])
 def upload():
     # Get the name of the uploaded file
+    global music_files,music_files_number
+#    music_files=[f for f in os.listdir('uploads/') if f.endswith('mp3')]
+ #   music_files_number = len(music_files)
+
     file = request.files['file']
+    print(type(file))
     # Check if the file is one of the allowed types/extensions
     if file and allowed_file(file.filename):
         # Make the filename safe, remove unsupported chars
@@ -47,11 +60,18 @@ def upload():
         # Move the file form the temporal folder to
         # the upload folder we setup
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        music_files.append(filename)
+        music_files_number += 1
+
         # Redirect the user to the uploaded_file route, which
         # will basicaly show on the browser the uploaded file
-        return redirect(url_for('uploaded_file',
-                                filename=filename))
-
+        return render_template('index.html',
+                               title='Home',
+                               music_files_number=music_files_number,
+                               music_files=music_files,
+                               ip_addr=request.remote_addr)
+        # return redirect('index.html')#url_for('uploaded_file',
+        #filename=filename))
 
 
 # This route is expecting a parameter containing the name
@@ -63,8 +83,23 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
+@app.route('/musicfile/', methods=['GET'])
+def musicfile():
+    global music_files_number,music_files
+    ret_data = {"music_files_number": music_files_number, "hell": "ji"}
+    x=""
+    for i in range(music_files_number):
+        x+=music_files[i]+"$$##"
+
+    ret_data["music_files"]=x
+
+    return jsonify(ret_data)
+
+
+
 if __name__ == '__main__':
     app.run(
+
         host="0.0.0.0",
         port=int("8080"),
         debug=True
--- static/music_player.js
+++ static/music_player.js
@@ -1,6 +1,9 @@
-var _player = document.getElementById("player"), 
+var _player = document.getElementById("player"),
 	_playlist = document.getElementById("playlist");
-
+//_player.loop=true;
+_player.src = _playlist.querySelector("li").getAttribute("data-mp3");
+_player.play();
+var current_li=_playlist.querySelector("li");
 
 function playlistItemClick(clickedElement) {
     var selected = _playlist.querySelector(".selected");
@@ -8,19 +11,35 @@ function playlistItemClick(clickedElement) {
         selected.classList.remove("selected");
     }
     clickedElement.classList.add("selected");
+    current_li=clickedElement;
 
     _player.src = clickedElement.getAttribute("data-mp3");
     _player.play();
 }
 
-function playNext() {
-    var selected = _playlist.querySelector("li.selected");
-    if (selected && selected.nextSibling) {
-        playlistItemClick(selected.nextSibling);
-    }
-}
+_player.addEventListener('ended', function() {
+      //  alert("OVERhello"+current_li.nextElementSibling.innerHTML);
 
-_player.addEventListener("ended", playNext);
+   //this.currentTime = 0;
+   if (current_li.nextElementSibling) {
+    //alert("OVERhelloworld"+current_li.nextElementSibling);
+
+        //this.currentSrc =current_li.nextElementSibling.getAttribute("data-mp3");
+        current_li=current_li.nextElementSibling;
+
+        //this.play();
+        //playlistItemClick(selected.nextSibling);
+    }
+    else
+    {
+        current_li=_playlist.querySelector("li");
+    }
+    this.src =current_li.getAttribute("data-mp3");
+    current_li.selected=true;
+    this.load();
+    this.play();
+}, false);
+
 _playlist.addEventListener("click", function (e) {
     if (e.target && e.target.nodeName === "LI") {
         playlistItemClick(e.target);
